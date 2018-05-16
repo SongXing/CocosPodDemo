df5340bdac96adabaf544852ac3d3d54922c5418
song <570158673@qq.com>
2018-05-16 10:27:09 +0800

    Initial Commit

---
+++ CocosPodDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocosPodDemo
+//
+//  Created by song on 2018/5/16.
+//  Copyright © 2018年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocosPodDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocosPodDemo
+//
+//  Created by song on 2018/5/16.
+//  Copyright © 2018年 song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
